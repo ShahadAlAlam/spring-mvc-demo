06fca5e598792f923f94564b5fe2cbc85f3e17a0
shahad.aust <shahad.aust@gmail.com>
2024-07-06 22:27:07 +0600

    working

--- src/main/java/com/saa/controller/DemoController.java
+++ src/main/java/com/saa/controller/DemoController.java
@@ -1,12 +1,13 @@
 package com.saa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DemoController {
-    @RequestMapping(value="/hello",method = RequestMethod.GET)
+    @RequestMapping("/hello")
     public String hello(){
 
         return "hello";
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,10 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+<div id="pageId">
+    <h3><a href="/">Index</a></h3>
+    <h3><a href="/hello">Hello</a></h3>
+</div>
 </body>
 </html>
